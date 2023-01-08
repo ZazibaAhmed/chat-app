5f3b55c2a4cd852007ad31f432c02762cc5c10b5
zaziba.ahmed <zaziba.ahmed@trucklagbe.com>
2023-01-08 16:08:26 +0600

    Basic d=socket.io setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const http = require('http');
 const path = require('path'); // core node module
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -9,6 +13,10 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 // Setup static directory to serve up
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-  })
+io.on('connection', () => {
+  console.log(`New WebSocket connection`)
+})
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
+})
