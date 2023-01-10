db14e9fe37917bfe7d4b3e4e7cb4e78452dabf2c
zaziba.ahmed <zaziba.ahmed@trucklagbe.com>
2023-01-10 16:57:54 +0600

    Location sharing done

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,3 +10,16 @@ document.querySelector('#message-form').addEventListener('submit', (event) => {
     socket.emit('sendMessage', message);
 });
 
+document.querySelector('#send-location').addEventListener('click', (event) => {
+   if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser');
+   }
+
+   navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', { 
+        latitude : position.coords.latitude, 
+        longitude : position.coords.longitude
+    });
+   }) 
+});
+
--- src/index.js
+++ src/index.js
@@ -26,6 +26,10 @@ io.on('connection', (socket) => { // runs for each different connection
     io.emit('message', message) 
   });
 
+  socket.on('sendLocation', (location) => {
+    io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`) 
+  });
+
   socket.on('disconnect', () => {
     io.emit('message', "A user has left");
   })
